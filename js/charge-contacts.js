e236c83e252a8f0c306368c1718a926793d9bd84
HasegawaTaizou <caiocoghi@gmail.com>
2023-03-20 23:00:43 -0300

    alo

---
+++ js/charge-contacts.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import { createChat } from "./create-chat.js";
+
+export const chargeContacts = async function (contatos) {
+  console.log(contatos);
+  const chatsContainer = document.querySelector(".chats__chats-container");
+  const chats = contatos.map(createChat);
+
+  chatsContainer.replaceChildren(...chats);
+};
